c105d5392ab5d05b02437ea5f3329e2b7109a21f
Glory Agatevure <agatevureglory@gmail.com>
2023-04-29 03:47:43 +0100

    fix proposal list

--- packages/react-app/components/TableList.tsx
+++ packages/react-app/components/TableList.tsx
@@ -9,7 +9,7 @@ import { convertEndTime, convertEnded, convertStartTime, dateToTimeStamp } from 
 
 export default function TableList() {
     
-    const proposals = useContractRead({
+    const proposals : any = useContractRead({
       address: CONTRACT_ADDRESS,
       abi: CONTRACT_ABI.abi,
       functionName: 'getAllProposals',
@@ -35,8 +35,7 @@ export default function TableList() {
     }
   }
   return (
-    <div>
-      
+    <div>     
         <h1 className='text-lg mt-4 font-bold'>List of Proposals</h1>
         <div className="flex flex-col">
           <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -49,12 +48,11 @@ export default function TableList() {
                       <th scope="col" className="px-6 py-4">Proposal</th>
                       <th scope="col" className="px-6 py-4">Creator Address</th>
                       <th scope="col" className="px-6 py-4">Voting Period</th>
-                    <th scope="col" className="px-6 py-4">Choice / Current Result</th>
-                    <th scope="col" className="px-6 py-4">Status</th>
+                      <th scope="col" className="px-6 py-4">Status</th>
                     </tr>
                   </thead>
                 <tbody>
-                  {proposals.data && proposals.data.map((item: any[], index: number) => 
+                  {proposals.data && proposals.data.map((item: any, index: number) => 
                       <tr key={index} onClick={() => Router.push({
                           pathname: `/proposals/${index}`,
                         query: {
@@ -68,23 +66,12 @@ export default function TableList() {
                         <td className="whitespace-nowrap px-6 py-4">{item.title}</td>
                         <td className="whitespace-nowrap px-6 py-4">{ item.creator}</td>
                         <td className="whitespace-nowrap px-6 py-4">{votingPeriod(item)}</td>
-                        <td className="whitespace-nowrap px-6 py-4 flex">
-                        <div onClick={() => write?.()} className='bg-slate-200 rounded-full px-2'>
-                          <label className='text-xl'>👍</label>
-                          <label className='text-lg'>{`${hexToNumber(item.yesVotes._hex)} %`}</label>
-                        </div>
-                        <div onClick={() => write?.()} className='bg-slate-200 rounded-full px-2 ml-2'>
-                          <label className='ml-4 text-xl'>👎</label> 
-                          <label className='text-lg'>{`${hexToNumber(item.noVotes._hex)} %`}</label>
-                        </div>
-                        </td>
                         <td>
                           <div className={` ${dateToTimeStamp() < hexToNumber(item.startTime._hex) ? 'bg-slate-200' : dateToTimeStamp() <= hexToNumber(item.endTime._hex)? "bg-green-200" : dateToTimeStamp() > hexToNumber(item.endTime._hex) ? "bg-red-200" : null}  rounded p-4`}>
                             {dateToTimeStamp() < hexToNumber(item.startTime._hex) ? 'Pending' : dateToTimeStamp() < hexToNumber(item.endTime._hex) ? "Active" : dateToTimeStamp() > hexToNumber(item.endTime._hex) ? "Closed" : null
                           }</div>
                         </td>
                       </tr>
-                    
                   )}
                 
                   </tbody>
